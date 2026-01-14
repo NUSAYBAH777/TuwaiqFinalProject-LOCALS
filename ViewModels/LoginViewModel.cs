40d412e15c0dfe654a23d82ce777728c4b28e63e
NUSAYBAH777 <111323060+NUSAYBAH777@users.noreply.github.com>
2026-01-14 20:20:12 +0300

    Files uploaded

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lLOCALS.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "يرجى إدخال البريد الإلكتروني")]
+        [EmailAddress(ErrorMessage = "صيغة البريد الإلكتروني غير صحيحة")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "يرجى إدخال كلمة المرور")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        // ميزة إضافية يمكنك تفعيلها لاحقاً
+        public bool RememberMe { get; set; }
+    }
+}
---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lLOCALS.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "الاسم الكامل مطلوب")]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "البريد الإلكتروني مطلوب")]
+        [EmailAddress(ErrorMessage = "صيغة البريد غير صحيحة")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "كلمة المرور مطلوبة")]
+        [MinLength(8, ErrorMessage = "يجب أن تكون 8 خانات على الأقل")] // قيد مستقل وسريع الفحص
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$",
+        ErrorMessage = "يجب أن تحتوي على حرف كبير، حرف صغير، رقم، ورمز خاص")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "يرجى تأكيد كلمة المرور")]
+        [Compare("Password", ErrorMessage = "كلمة المرور غير متطابقة")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
+
+
